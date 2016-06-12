1c6e84a348d8697d23f69660034fe47690a13ded
Pavel <side-by-side@users.noreply.github.com>
2016-06-12 16:41:55 +0300

    Added russian translation

---
+++ lang/ru/lang.php
@@ -0,0 +1,65 @@
+<?php
+
+return [
+    'strings' => [
+        'plugin_description' => 'Дополнительные виджеты для Google Analytics.',
+        'barchart_label' => 'Google Analytics дополнительный график',
+        'percentage_label' => 'Google Analytics дополнительный процентный график',
+        'piechart_label' => 'Google Analytics дополнительный круговой график',
+    ],
+
+    'barchart' => [
+        'widget_title' => 'Заголовок виджета',
+        'bar_chart' => 'График',
+        'widget_title_required' => 'Заголовок виджета должен быть указан.',
+        'dimension' => 'Составляющая',
+        'invalid_dimension' => 'Неверная составляющая.',
+        'metric' => 'Метрика',
+        'invalid_metric' => 'Неверная метрика.',
+        'chart_height' => 'Высота графика',
+        'invalid_chart_height' => 'Пожалуйста укажите высоту графика в виде числа',
+        'legend_as_table' => 'Легенда в виде таблицы',
+        'days_to_display' => 'Количество дней',
+        'results_to_display' => 'Количество результатов',
+        'zero_displays_all' => 'Значение 0 выведет все результаты.',
+        'raport_description' => 'Описание отчета',
+    ],
+    'percentage' => [
+        'widget_title' => 'Заголовок виджета',
+        'percentage_chart' => 'Процентный график',
+        'title_required' => 'Заголовок виджета должен быть указан.',
+        'dimension' => 'Составляющая',
+        'invalid_dimension' => 'Неверная составляющая.',
+        'metric' => 'Метрика',
+        'invalid_metric' => 'Неверная метрика.',
+        'dimension_label' => 'Название составляющей',
+        'metric_label' => 'Название метрики',
+        'days_to_display' => 'Количество дней',
+        'results_to_display' => 'Количество результатов',
+        'zero_displays_all' => 'Значение 0 выведет все результаты.',
+    ],
+    'piechart' => [
+        'widget_title' => 'Заголовок виджета',
+        'pie_chart' => 'Круговой график',
+        'title_required' => 'Заголовок виджета должен быть указан.',
+        'dimension' => 'Составляющая',
+        'invalid_dimension' => 'Неверная составляющая.',
+        'metric' => 'Метрика',
+        'invalid_metric' => 'Неверная метрика.',
+        'chart_radius' => 'Радиус графика',
+        'chart_size_invalid' => 'Пожалуйста укажите радиус графика в виде числа',
+        'center_chart' => 'График по центру',
+        'legend_as_table' => 'Легенда в виде таблицы',
+        'display_total' => 'Показывать всего',
+        'days_to_display' => 'Количество дней',
+        'results_to_display' => 'Количество результатов',
+        'zero_displays_all' => 'Значение 0 выведет все результаты.',
+        'raport_description' => 'Описание отчета',
+    ],
+    'errors' => [
+        'invalid_days' => 'Неверное значение дней: ',
+        'invalid_dimension' => 'Неверное значение составляющей: ',
+        'invalid_metric' => 'Неверное значение метрики: ',
+    ]
+
+];
